a438627d21bc0e2b8e466f6cf2d6d5de6955ca4f
Adam Griffiths <107414001+adamgriff86@users.noreply.github.com>
2023-09-12 11:52:04 +0200

    add proxy middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import Koa from 'koa';
 import parser from 'koa-bodyparser';
 import cors from 'koa-cors';
+import proxy from 'koa-proxies';
 import { Server } from 'socket.io';
 import http from 'http';
 import dotenv from 'dotenv';
@@ -24,20 +25,28 @@ const serverOptions = {
 const io = new Server(server, serverOptions);
 
 // Middlewares
-// app.use(cors({
-//   origin: process.env.CLIENT_URL,
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   headers: ['Origin', 'Content-Type', 'Accept', 'Authorization', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Credentials'],
-//   credentials: true,
-// }));
+app.use(
+  proxy('/', {
+    target: 'https://lendify-production.up.railway.app',
+    changeOrigin: true,
+    logs: true,
+  })
+);
 
-app.use(async (ctx, next) => {
-  ctx.set('Access-Control-Allow-Origin', 'https://grand-tapioca-07d51d.netlify.app');
-  ctx.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  ctx.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  ctx.set('Access-Control-Allow-Credentials', 'true');
-  await next();
-});
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  headers: ['Origin', 'Content-Type', 'Accept', 'Authorization', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Credentials'],
+  credentials: true,
+}));
+
+// app.use(async (ctx, next) => {
+//   ctx.set('Access-Control-Allow-Origin', 'https://grand-tapioca-07d51d.netlify.app');
+//   ctx.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   ctx.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   ctx.set('Access-Control-Allow-Credentials', 'true');
+//   await next();
+// });
 
 app.use(parser());
 app.use(router.routes());
